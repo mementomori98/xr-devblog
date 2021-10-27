236ab8ad82b9b67f31656b6ae532d4f83c00e810
Mantas Miklasevicius <43003579+Mintiss@users.noreply.github.com>
2021-10-27 20:07:25 +0300

    Updated blog week 3

--- src/Posts/Week3.svelte
+++ src/Posts/Week3.svelte
@@ -12,4 +12,20 @@
         Adding spaces<br/>
         Lose / Win screen<br/>
     </Text>
+    <Text>
+        In the second week of the marker based VR project we managed to finish up our Paddle game project.
+    </Text>
+    <Text>
+        As we had a preset map for the game, we decided that we do not want to have just a bunch of prefabs copied around the playing area, as it
+        would be a bad design choice for future game improvements like adding more levels. We implemented this by generating the prefabs and setting the bounds so they wouldn't
+        be outside the play space
+    </Text>
+    <Text>
+        Adding spaces??? wat
+    </Text>
+    <Text>
+        We also implemented a win and lose screen if the player manages to either hit all the blocks and win or miss the ball with the paddle and lose. This was made with a counter
+        for the block which reaches a certain count and triggers the win game screen or if you miss the ball and it goes out of bound of the play area, it triggers the lose screen
+        and restarts the game.
+    </Text>
 </Post>
